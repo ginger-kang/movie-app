378d708c978de8ba65d10e71f308ce5f901bf6a8
ginger-kang <kdhoon07@gmail.com>
2020-04-24 02:08:32 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, rating }) {
-return (
-  <div>
-    <h1>I like { name }</h1>
-    <h4>{rating}/5.0</h4>
-  </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
-const foodILike = [
-  {
-    id:1,
-    name:"saba",
-    rating: 5
-  },
-  {
-    id:2,
-    name:"katsudon",
-    rating: 4.8
-  },
-  {
-    id:3,
-    name:"pizza",
-    rating:4.7
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
   }
-]
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+       {isLoading ? "Loading..." : "We are ready"}
+      </div>
+    );
+  }
 }
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} rating={dish.rating}/>)}
-      
-    </div>
-  );
-}
-
-export default App;
+export 
+default App;
