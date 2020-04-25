12fa1df16ed806c29634083b59628f4669876be1
ginger-kang <kdhoon07@gmail.com>
2020-04-26 03:33:19 +0900

    Updates

---
+++ precache-manifest.91ad7a8af3e398b16f0a68d945d699ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fbb944e765f1a6d99abec26a2cfb7f",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "67f1363f743ae451e42f",
+    "url": "/movie-app/static/css/main.200a3e3a.chunk.css"
+  },
+  {
+    "revision": "b7fab0088a1b6927b8ae",
+    "url": "/movie-app/static/js/2.fa1f7183.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.fa1f7183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f1363f743ae451e42f",
+    "url": "/movie-app/static/js/main.7c5d18ee.chunk.js"
+  },
+  {
+    "revision": "ef13b2d46e7e451c8c9d",
+    "url": "/movie-app/static/js/runtime-main.4744ba8c.js"
+  }
+]);
